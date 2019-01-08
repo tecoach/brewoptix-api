ce00e9ca05317ca29ceb61c01bff5cc3b5361d30
root <mateusbp.up@hotmail.com>
2019-01-08 11:51:12 -0300

    29_BREWAPI_164.sh added

---
+++ deployment_scripts/29_BREWAPI_164.sh
@@ -0,0 +1,43 @@
+STAGE=$1
+
+# this snippet can be used in other scripts to check if already deployed
+echo "Check if script number is already added to deployment table"
+cd $PWD/deployment_scripts/common || exit
+python check_script_number_in_deployment_table.py $STAGE 29
+if [ $? = 1 ]; then
+    echo "Script already run. Exiting..."
+    exit 1
+fi
+cd ../.. || exit
+
+echo "Deploy q_production service"
+cd $PWD/services/q_production || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+    exit 1
+fi
+
+cd ../.. || exit
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+fi
+
+echo "Add script number to deployment table"
+cd $PWD/deployment_scripts/common || exit
+python add_script_number_to_deployment_table.py $STAGE 29
+
+if [ $? = 0 ]; then
+    echo "Issue id successfully added to deployment table"
+else
+    echo "Issue already exists or deployment table doesn't exist. Exiting..."
+    exit 1
+fi
+
+cd ../.. || exit
