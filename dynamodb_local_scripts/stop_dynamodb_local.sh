c27d4d08ba076aef064c5b193a439cd3fc5b887b
root <mateusbp.up@hotmail.com>
2019-01-18 18:33:38 -0300

    stop_dynamodb_local.sh updated

--- dynamodb_local_scripts/stop_dynamodb_local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Sourcing dynamo local config variables
-# Please make sure the initialize and stop script are run from parent directory
-. dynamodb_local_scripts/dynamodb_local_config.sh
-
-if [ "$(docker ps -q -f name=$DYNAMO_LOCAL_CONTAINER_NAME)" ]; then
-    docker stop $DYNAMO_LOCAL_CONTAINER_NAME
-fi
