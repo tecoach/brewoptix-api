557db3faed774a18d76d290b2dc90b9c9a7434c1
root <mateusbp.up@hotmail.com>
2019-05-16 11:48:09 -0300

    34_BREWAPI_202.sh added

---
+++ deployment_scripts/34_BREWAPI_202.sh
@@ -0,0 +1,74 @@
+STAGE=$1
+
+# this snippet can be used in other scripts to check if already deployed
+echo "Check if script number is already added to deployment table"
+cd $PWD/deployment_scripts/common || exit
+python check_script_number_in_deployment_table.py $STAGE 34
+if [ $? = 1 ]; then
+    echo "Script already run. Exiting..."
+    exit 1
+fi
+cd ../.. || exit
+
+echo "Deploy supplier_distributors service"
+cd $PWD/services/supplier_distributors || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+    exit 1
+fi
+
+cd ../.. || exit
+
+echo "Deploy purchase_orders service"
+cd $PWD/services/purchase_orders || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+    exit 1
+fi
+
+cd ../.. || exit
+
+echo "Deploy suppliers service"
+cd $PWD/services/suppliers || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+    exit 1
+fi
+
+cd ../.. || exit
+
+echo "Deploy distributors service"
+cd $PWD/services/distributors || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+    exit 1
+fi
+
+cd ../.. || exit
+
+echo "Add script number to deployment table"
+cd $PWD/deployment_scripts/common || exit
+python add_script_number_to_deployment_table.py $STAGE 34
+
+if [ $? = 0 ]; then
+    echo "Script number successfully added to deployment table"
+else
+    echo "Script number already exists or deployment table doesn't exist. Exiting..."
+    exit 1
+fi
