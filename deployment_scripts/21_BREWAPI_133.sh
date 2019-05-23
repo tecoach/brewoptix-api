6fc9c08be06f73f6e958a01f9689a10a20ebf898
root <mateusbp.up@hotmail.com>
2019-05-23 11:16:42 -0300

    21_BREWAPI_133.sh added

---
+++ deployment_scripts/21_BREWAPI_133.sh
@@ -0,0 +1,13 @@
+STAGE=$1
+
+echo "Deploy suppliers service"
+cd $PWD/services/suppliers || exit
+sls deploy -v --stage $STAGE
+
+if [ $? = 0 ]; then
+    echo "Successfully deployed"
+else
+    echo "Deployment failed"
+fi
+
+cd ../.. || exit
