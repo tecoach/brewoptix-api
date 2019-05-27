a871f92dc452f45a6d523d2952eb7360de104a26
root <mateusbp.up@hotmail.com>
2019-05-27 10:02:26 -0300

    api_constants.py updated

--- services/api_constants.py
+++
@@ -1,9 +0,0 @@
-# Global Constants
-
-# Metadata associated with all data stored
-base_attributes = ['entity_id', 'version', 'previous_version',
-                   'active', 'latest', 'changed_by_id',
-                   'changed_on']
-
-# Attributes used only for tracking and internal use
-private_base_attributes = ['previous_version', 'active', 'latest']
